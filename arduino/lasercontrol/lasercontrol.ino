4f64fe74a0a9d04fa9e28124606e794f440065d4
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-05-03 16:52:32 -0300

    tabs

--- arduino/lasercontrol/lasercontrol.ino
+++ arduino/lasercontrol/lasercontrol.ino
@@ -35,7 +35,7 @@ int average5 = 0;
 //ADD ARRAY OF RECEIVERS
 //CONTROL STEPER SPEED
 
-int lasers[]= {3, 4, 5, 6, 9};
+int lasers[] = {3, 4, 5, 6, 9};
 const byte POTENTIOMETER = 6;
 const byte MOTORCONTROL = 7;
 const byte MOTOR = 10;
@@ -70,13 +70,13 @@ void setup() {
   Serial.begin(9600);
   Serial.println("stepper runing");
   stepper.setSpeed(30);
-  
+
   pinMode(LASER, OUTPUT);
   pinMode(LASER2, OUTPUT);
   pinMode(LASER3, OUTPUT);
   pinMode(LASER4, OUTPUT);
   pinMode(LASER5, OUTPUT);
-  
+
   int sum = 0;
   int sum2 = 0;
   int sum3 = 0;
@@ -85,130 +85,130 @@ void setup() {
 
 
   for (int thisReading = 0; thisReading < numReadings; thisReading++)
-  {  readings[thisReading] = 0; 
-   sum = sum + analogRead(inputPin);//hacer lectura del sensor;
-    delay(1);
-  }
-  
-  
-  for (int thisReading = 0; thisReading < numReadings; thisReading++)
-  {  readings2[thisReading] = 0; 
-   sum2 = sum2 + analogRead(inputPin2);//hacer lectura del sensor;
-    delay(1);
-  }
-  
-  for (int thisReading = 0; thisReading < numReadings; thisReading++)
-  {  readings3[thisReading] = 0; 
-   sum3 = sum3 + analogRead(inputPin3);//hacer lectura del sensor;
-    delay(1);
-  }
-  
-  for (int thisReading = 0; thisReading < numReadings; thisReading++)
-  {  readings4[thisReading] = 0; 
-   sum4 = sum4 + analogRead(inputPin4);//hacer lectura del sensor;
-    delay(1);
-  }
-  
-  for (int thisReading = 0; thisReading < numReadings; thisReading++)
-  {  readings5[thisReading] = 0; 
-   sum5 = sum5 + analogRead(inputPin5);//hacer lectura del sensor;
+  { readings[thisReading] = 0;
+    sum = sum + analogRead(inputPin);//hacer lectura del sensor;
     delay(1);
   }
 
-ambientLight = sum / numReadings;
-ambientLight2 = sum2 / numReadings;
-ambientLight3 = sum3 / numReadings;
-ambientLight4 = sum4 / numReadings;
-ambientLight5 = sum5 / numReadings;
+
+  for (int thisReading = 0; thisReading < numReadings; thisReading++)
+  { readings2[thisReading] = 0;
+    sum2 = sum2 + analogRead(inputPin2);//hacer lectura del sensor;
+    delay(1);
+  }
+
+  for (int thisReading = 0; thisReading < numReadings; thisReading++)
+  { readings3[thisReading] = 0;
+    sum3 = sum3 + analogRead(inputPin3);//hacer lectura del sensor;
+    delay(1);
+  }
+
+  for (int thisReading = 0; thisReading < numReadings; thisReading++)
+  { readings4[thisReading] = 0;
+    sum4 = sum4 + analogRead(inputPin4);//hacer lectura del sensor;
+    delay(1);
+  }
+
+  for (int thisReading = 0; thisReading < numReadings; thisReading++)
+  { readings5[thisReading] = 0;
+    sum5 = sum5 + analogRead(inputPin5);//hacer lectura del sensor;
+    delay(1);
+  }
+
+  ambientLight = sum / numReadings;
+  ambientLight2 = sum2 / numReadings;
+  ambientLight3 = sum3 / numReadings;
+  ambientLight4 = sum4 / numReadings;
+  ambientLight5 = sum5 / numReadings;
 
 
 }
 
-void loop() { 
+void loop() {
 
   Serial.println("Forward");
   stepper.step(STEPS);
   //Serial.println("Backward");
   //stepper.step(-STEPS);
-   
-    //Laser control 
+
+  //Laser control
   reading = analogRead(POTENTIOMETER);
-  value = map(reading, 0, 1024, 0, 255);   
+  value = map(reading, 0, 1024, 0, 255);
   analogWrite(LASER, value);
-    analogWrite(LASER2, value2);
-      analogWrite(LASER3, value3);
-        analogWrite(LASER4, value4);
-          analogWrite(LASER5, value5);
+  analogWrite(LASER2, value2);
+  analogWrite(LASER3, value3);
+  analogWrite(LASER4, value4);
+  analogWrite(LASER5, value5);
 
 
-  //Sensor smoothing 
-  total= total - readings[Myindex];         
-  readings[Myindex] = analogRead(1) - ambientLight; 
-  total= total + readings[Myindex];       
-  Myindex = Myindex + 1;  
+  //Sensor smoothing
+  total = total - readings[Myindex];
+  readings[Myindex] = analogRead(1) - ambientLight;
+  total = total + readings[Myindex];
+  Myindex = Myindex + 1;
 
-  //Sensor smoothing 
-  total2= total2 - readings2[Myindex2];         
-  readings2[Myindex2] = analogRead(2) - ambientLight2; 
-  total2= total2 + readings2[Myindex2];       
-  Myindex2 = Myindex2 + 1;  
-  
-    //Sensor smoothing 
-  total3= total3 - readings3[Myindex3];         
-  readings3[Myindex3] = analogRead(3) - ambientLight3; 
-  total3= total3 + readings3[Myindex3];       
+  //Sensor smoothing
+  total2 = total2 - readings2[Myindex2];
+  readings2[Myindex2] = analogRead(2) - ambientLight2;
+  total2 = total2 + readings2[Myindex2];
+  Myindex2 = Myindex2 + 1;
+
+  //Sensor smoothing
+  total3 = total3 - readings3[Myindex3];
+  readings3[Myindex3] = analogRead(3) - ambientLight3;
+  total3 = total3 + readings3[Myindex3];
   Myindex3 = Myindex3 + 1;
-  
-    //Sensor smoothing 
-  total4= total4 - readings4[Myindex4];         
-  readings4[Myindex4] = analogRead(4) - ambientLight4; 
-  total4= total4 + readings4[Myindex4];       
+
+  //Sensor smoothing
+  total4 = total4 - readings4[Myindex4];
+  readings4[Myindex4] = analogRead(4) - ambientLight4;
+  total4 = total4 + readings4[Myindex4];
   Myindex4 = Myindex4 + 1;
-  
-    //Sensor smoothing 
-  total5= total5 - readings5[Myindex5];         
-  readings5[Myindex5] = analogRead(5) - ambientLight5; 
-  total5= total5 + readings5[Myindex5];       
+
+  //Sensor smoothing
+  total5 = total5 - readings5[Myindex5];
+  readings5[Myindex5] = analogRead(5) - ambientLight5;
+  total5 = total5 + readings5[Myindex5];
   Myindex5 = Myindex5 + 1;
 
-  if (Myindex >= numReadings)              
-    Myindex = 0;  
+  if (Myindex >= numReadings)
+    Myindex = 0;
 
-  if (Myindex2 >= numReadings)              
+  if (Myindex2 >= numReadings)
     Myindex2 = 0;
 
-  if (Myindex3 >= numReadings)              
+  if (Myindex3 >= numReadings)
     Myindex3 = 0;
 
-   if (Myindex4 >= numReadings)              
-    Myindex4 = 0;  
+  if (Myindex4 >= numReadings)
+    Myindex4 = 0;
 
-   if (Myindex5 >= numReadings)              
-    Myindex5 = 0;                                 
+  if (Myindex5 >= numReadings)
+    Myindex5 = 0;
 
   // calculate the average and print:
-  average = total / numReadings; 
-  average2 = total2 / numReadings; 
-  average3 = total3 / numReadings;         
-  average4 = total4 / numReadings;         
-  average5 = total5 / numReadings;         
-    
-        
-  Serial.print(average); 
-  Serial.print("\t Sensor1:");  
-  Serial.print(average2); 
-  Serial.print("\t Sensor2:");  
-  Serial.print(average3); 
-  Serial.print("\t Sensor3:");  
-  Serial.print(average4); 
-  Serial.print("\t Sensor4:");  
-  Serial.print(average5); 
-  Serial.print("\t Sensor5:");  
-  Serial.println();                 
+  average = total / numReadings;
+  average2 = total2 / numReadings;
+  average3 = total3 / numReadings;
+  average4 = total4 / numReadings;
+  average5 = total5 / numReadings;
+
+
+  Serial.print(average);
+  Serial.print("\t Sensor1:");
+  Serial.print(average2);
+  Serial.print("\t Sensor2:");
+  Serial.print(average3);
+  Serial.print("\t Sensor3:");
+  Serial.print(average4);
+  Serial.print("\t Sensor4:");
+  Serial.print(average5);
+  Serial.print("\t Sensor5:");
+  Serial.println();
   delay(10);
 
-   
-  //agregar libreria y controlar el motor por driver 
+
+  //agregar libreria y controlar el motor por driver
 
 }
 
